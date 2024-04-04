0dd25414acf896018ad92e90c2914b2b28f40783
Anton de Groot <aammdegroot@gmail.com>
2024-04-04 10:00:20 +0200

    001 add backend structure

---
+++ Backend/src/main/java/com/example/demo/HolidayResortApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayResortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayResortApplication.class, args);
+	}
+
+}
